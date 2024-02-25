7e83efa1c971de4046dbfada4e0f4fe5fc7153a5
Raj soni <74926805+rajsoni5197@users.noreply.github.com>
2024-02-25 21:21:39 +0530

    Update WinState.jsx
    adding sound

--- store/WinState.jsx
+++ store/WinState.jsx
@@ -1,6 +1,8 @@
 import { createContext, useContext, useState } from "react";
 import { BoardStateContext } from "./BoardState";
-import winingCondition from './winCondition.json'
+import winingCondition from './winCondition.json';
+import draw from '/Draw_sound.wav'
+import win from '/win_sound.mp3'
 
 export const currentWinState = createContext();
 
@@ -10,12 +12,19 @@ function WinStateProvider({children}) {
     const [winCombination , setWinCombination] = useState(null);
     const {boardState} = useContext(BoardStateContext);
     
+    
     function isWinner(){
         for (const combination of winingCondition.winCondition) {
             const [a, b, c] = combination;
             // Check if all squares are not null/undefined and have the same value
             if (boardState[a] && boardState[a] === boardState[b] && boardState[a] === boardState[c]) {
                 setWinCombination(combination[3]);
+                try{
+                    new Audio(win).play();
+                }
+                catch(error){
+                    console.log(error);
+                }
                 return boardState[a]; // Return the winning symbol
 
             }
@@ -27,6 +36,13 @@ function WinStateProvider({children}) {
         {
             if(!ele) return null;
         }
+        try{
+            new Audio(draw).play();
+        }
+        catch(error){
+            console.log(error);
+        }
+        
         return 'd';
    }
     
